51eed4d2acffc2525cc2884f645d4cd7fd0f0d85
alin.baicu <alin.baicu@teamnet.ro>
2015-11-26 12:44:33 +0200

    My fist commit

---
+++ main/src/main/java/MyFirstClass.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Alin.Baicu on 11/26/2015.
+ */
+public class MyFirstClass {
+
+    public static void main(String[] args) {
+        System.out.println("My first java class");
+    }
+}
